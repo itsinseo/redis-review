3a7efce44eb482797eef0751ec2fae1537de077f
Inseo Kim <itsinseo@gmail.com>
2023-11-28 19:36:17 +0900

    git init

---
+++ src/main/java/com/example/redisreview/RedisReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redisreview/RedisReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redisreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

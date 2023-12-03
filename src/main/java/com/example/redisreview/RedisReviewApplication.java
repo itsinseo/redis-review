7cdd0f68ce172a321f4b4cf30632445727c6dc45
Inseo Kim <itsinseo@gmail.com>
2023-12-03 18:59:55 +0900

    Redis Pub/Sub을 이용한 채팅방 구현 (Spring CommandLineRunner 이용)

--- src/main/java/com/example/redisreview/RedisReviewApplication.java
+++ src/main/java/com/example/redisreview/RedisReviewApplication.java
@@ -1,13 +1,27 @@
 package com.example.redisreview;
 
+import com.example.redisreview.service.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedisReviewApplication {
+public class RedisReviewApplication implements CommandLineRunner {
+
+	@Autowired
+	private ChatService chatService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedisReviewApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String chatRoomName = "room1";
+
+		System.out.println("Redis Chat Application started");
+
+		chatService.enterChatRoom(chatRoomName);
+	}
 }
